47f23b4853d7556b44df143201b021d54a5e02ff
James Puryear <jpuryear@gmx.com>
2017-06-11 13:28:55 -0700

    Fix player ground sensor colliding with things other than platforms

--- src/entities/heroes/setPhysics.js
+++ src/entities/heroes/setPhysics.js
@@ -2,12 +2,13 @@ module.exports = function setPhysics(player) {
     var state = player.state
 
     player.game.physics.p2.enable(player)
-    player.body.setRectangle(player.character.width/2, player.character.height)
+    var s = player.body.setRectangle(player.character.width/2, player.character.height)
 
     var groundSensor = player.body.addRectangle(
         player.character.width*2/3, 2, 0, player.character.height/2
     )
     groundSensor.sensor = true
+    player.body.collideWorldBounds = false
 
     player.body.onBeginContact.add(function(){
         if(arguments[2] === groundSensor) player.standing++
@@ -16,10 +17,11 @@ module.exports = function setPhysics(player) {
         if(arguments[2] === groundSensor) player.standing--
     }, player)
 
-    player.body.setMaterial(state.playerMaterial);
-    player.body.setCollisionGroup(state.playersCG);
-    player.body.collides(state.enemiesCG, player.damage, player);
-    player.body.collides([state.itemsCG, state.platformsCG]);
+    player.body.setMaterial(state.playerMaterial)
+    player.body.setCollisionGroup(state.playersCG)
+    player.body.collides(state.platformsCG, null, null, groundSensor)
+    player.body.collides(state.enemiesCG, player.damage, player, s)
+    player.body.collides([state.itemsCG, state.platformsCG, state.physics.p2.boundsCollisionGroup], null, null, s)
 
     player.body.fixedRotation = true
 }
