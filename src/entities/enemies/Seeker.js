efeca1d483b55806881c673930f174275527a7c2
James Puryear <jpuryear@gmx.com>
2017-07-19 23:59:56 -0700

    Fix repel buff failing to work on seeker enemies

--- src/entities/enemies/Seeker.js
+++ src/entities/enemies/Seeker.js
@@ -3,19 +3,18 @@ module.exports = Seeker;
 
 var Enemy = require('./Enemy.js');
 
-var ACCEL = 2;
+var ACCEL = 140;
+var ACCEL_SQR = ACCEL*ACCEL
 var PREFER_SPEED = 80;
 
 
 function Seeker(state, data, drop) {
     Enemy.call(this, state, data, drop);
     this.targets = state.players;
-    this.accel = ACCEL;
     this.body.data.gravityScale = 0;
     this.body.mass = 0.5;
     // Tying speed to mass makes slowmo work.
     this._preferSpeed = this.body.mass*PREFER_SPEED;
-    this.body.removeCollisionGroup(state.platformsCG);
 }
 
 
@@ -28,38 +27,29 @@ Seeker.prototype.defaultFrame = 'seeker';
 
 Seeker.prototype.update = function() {
     Enemy.prototype.update.apply(this, arguments);
+
     var target = this.targets.getClosestTo(this);
     if (!target) return;
-    var goRight = target.world.x >= this.world.x ? true : false;
-    var goDown = target.world.y >= this.world.y ? true : false;
+
+    var theta = this.world.angle(target.world)
     var vel = this.body.velocity;
     var max = this.preferSpeed;
-    var accel = this.accel;
-    // TODO: SRSLY!!! Do we need all this?
-    if (goRight === true) {
-        if (vel.x > max) {
-            vel.x -= accel;
-        } else {
-            vel.x = Math.min(vel.x + accel, max);
-        }
-    } else {
-        if (vel.x < -max) {
-            vel.x += accel;
-        } else {
-            vel.x = Math.max(vel.x - accel, -max);
-        }
+    var desired = {
+        x: max * Math.cos(theta),
+        y: max * Math.sin(theta)
+    };
+    var steer = {
+        x: desired.x - vel.x,
+        y: desired.y - vel.y
+    };
+    var steerSqr = steer.x*steer.x + steer.y*steer.y;
+    if (steerSqr > ACCEL_SQR) {
+        var scale = ACCEL/Math.sqrt(steerSqr)
+        steer.x *= scale;
+        steer.y *= scale;
     }
-    if (goDown === true) {
-        if (vel.y > max) {
-            vel.y -= accel;
-        } else {
-            vel.y = Math.min(vel.y + accel, max);
-        }
-    } else {
-        if (vel.y < -max) {
-            vel.y += accel;
-        } else {
-            vel.y = Math.max(vel.y - accel, -max);
-        }
-    }
+    this.body.data.applyForce([
+        this.body.world.pxm(-steer.x * this.body.mass),
+        this.body.world.pxm(-steer.y * this.body.mass)
+    ]);
 }
