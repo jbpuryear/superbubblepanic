2ee1838fd41096649fa921bf1184bd37a8906284
James Puryear <jpuryear@gmx.com>
2017-08-13 07:44:51 -0700

    Better jetpack particles

--- src/entities/heroes/PlayerFX.js
+++ src/entities/heroes/PlayerFX.js
@@ -4,6 +4,7 @@ module.exports = PlayerFX
 function PlayerFX(state) {
     this.dust = state.add.emitter(0, 0, 10)
     this.flame = state.add.emitter(0, 0, 30)
+    this.exhaust = state.add.emitter(0, 0, 10)
 
     this.dust.makeParticles('sprites', 
         Phaser.Animation.generateFrameNames('dust', 1, 4))
@@ -22,4 +23,13 @@ function PlayerFX(state) {
     this.flame.setXSpeed(-40, 40)
     this.flame.setYSpeed(60, 80)
     this.flame.lifespan = 400
+
+    this.exhaust.makeParticles('sprites', 
+        Phaser.Animation.generateFrameNames('dust', 1, 4))
+    this.exhaust.setScale(0.5, 2, 0.5, 2, 400)
+    this.exhaust.setRotation(0)
+    this.exhaust.setXSpeed(-80, 80)
+    this.exhaust.setYSpeed(-10, 100)
+    this.exhaust.setAlpha(1, 0.2, 400)
+    this.exhaust.setScale(0.25, 1, 0.25, 1, 200)
 }
