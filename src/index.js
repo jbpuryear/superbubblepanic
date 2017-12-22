91a5c20858f4b3143cad3b960f1f412242fc7f11
James Puryear <jpuryear@gmx.com>
2017-12-21 20:51:08 -0800

    Move nineslice lib, better build process

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-module.exports = Game;
-
-
 var GameData = require('./GameData.js')
 var Boot = require('./boot.js');
 var Load = require('./load.js');
@@ -16,31 +13,27 @@ var FinalBoss = require('./level/FinalBoss.js');
 var Arcade = require('./arcade/arcade.js');
 var Credits = require('./Credits.js');
 
+require('./phaserPatch.js')();
 
-function Game() {
-    require('./phaserPatch.js')();
+var game = new Phaser.Game(800, 600, undefined, undefined, undefined, false, false)
 
-    var game = new Phaser.Game(800, 600, undefined,
-        undefined, undefined, false, false)
+game.data = new GameData(game)
 
-    game.data = new GameData(game)
+game.state.add('Boot', new Boot);
+game.state.add('Load', new Load);
+game.state.add('Level', new Level);
+game.state.add('RocketLevel', new RocketLevel);
+game.state.add('ZeroGLevel', new ZeroGLevel);
+game.state.add('ShmupLevel', new ShmupLevel);
+game.state.add('MonsterLevel', new MonsterLevel);
+game.state.add('SpaceBoss', new SpaceBoss);
+game.state.add('FinalBoss', new FinalBoss);
+game.state.add('Menu', new Menu);
+game.state.add('Arcade', new Arcade);
+game.state.add('LevelSelect', new LevelSelect);
+game.state.add('Credits', new Credits);
 
-    game.state.add('Boot', new Boot);
-    game.state.add('Load', new Load);
-    game.state.add('Level', new Level);
-    game.state.add('RocketLevel', new RocketLevel);
-    game.state.add('ZeroGLevel', new ZeroGLevel);
-    game.state.add('ShmupLevel', new ShmupLevel);
-    game.state.add('MonsterLevel', new MonsterLevel);
-    game.state.add('SpaceBoss', new SpaceBoss);
-    game.state.add('FinalBoss', new FinalBoss);
-    game.state.add('Menu', new Menu);
-    game.state.add('Arcade', new Arcade);
-    game.state.add('LevelSelect', new LevelSelect);
-    game.state.add('Credits', new Credits);
+game.state.start('Boot');
 
-    game.state.start('Boot');
-
-    return game;
-}
+window.game = game
 
