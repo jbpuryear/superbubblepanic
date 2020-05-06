ca32bb72b19793185fa20c69f765f47a6d4398be
James Puryear <jpuryear@gmx.com>
2020-05-05 22:48:03 -0400

    Fix song loop for heartbeat

--- game.bundle.js
+++ game.bundle.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 module.exports={
     "audio": [
         {
@@ -195,7 +195,7 @@ module.exports={
                     },
                     "loop": {
                         "start": 6.388,
-                        "end": 108.75,
+                        "end": 57.517,
                         "loop": true
                     }
                 }
@@ -216,26 +216,6 @@ module.exports={
                     "loop": {
                         "start": 8.0,
                         "end": 104.003,
-                        "loop": true
-                    }
-                }
-            }
-        },
-        {
-            "type": "audiosprite",
-            "key": "N35-40-19-800",
-            "urls": ["assets/songs/springtide-N35-40-19-800.ogg"],
-            "autoDecode": false,
-            "jsonData": {
-                "spritemap": {
-                    "intro" : {
-                        "start": 0,
-                        "end": 3.356,
-                        "loop": false
-                    },
-                    "loop": {
-                        "start": 3.356,
-                        "end": 210.378,
                         "loop": true
                     }
                 }
@@ -939,7 +919,6 @@ module.exports={
         "bgImage": "desert",
         "songs": [
             "everything-is-changing",
-            "N35-40-19-800",
             "spring-summer"
         ],
         "tiles": "tiles"
